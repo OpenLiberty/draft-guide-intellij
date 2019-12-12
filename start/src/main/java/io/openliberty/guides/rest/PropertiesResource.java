6f686d586c2e5143a53c92b7eaaa284e198a89cb
meswan <meswan@ca.ibm.com>
2019-12-12 14:31:14 -0500

    Add test section

--- start/src/main/java/io/openliberty/guides/rest/PropertiesResource.java
+++ start/src/main/java/io/openliberty/guides/rest/PropertiesResource.java
@@ -12,14 +12,26 @@
 // end::comment[]
 package io.openliberty.guides.rest;
 
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
+import java.util.Properties;
 
-// tag::applicationPath[]
-@ApplicationPath("System")
-// end::applicationPath[]
-// tag::systemApplication[]
-public class SystemApplication extends Application {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+// tag::path[]
+@Path("properties")
+// end::path[]
+public class PropertiesResource {
+
+    // tag::get[]
+    @GET
+    // end::get[]
+    // tag::produces[]
+    @Produces(MediaType.APPLICATION_JSON)
+    // end::produces[]
+    public Properties getProperties() {
+        return System.getProperties();
+    }
 
 }
-// end::systemApplication[]
